76e3bb6f7673add0cf2cba9a0f37020317a7d267
Sanjeev Thapa <116951750+sanjeevlcc@users.noreply.github.com>
2024-03-16 11:26:36 +0000

    v10

---
+++ java_script_workload/basics/c_vue/vue-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World from Vue.js!'
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+</style>
+
