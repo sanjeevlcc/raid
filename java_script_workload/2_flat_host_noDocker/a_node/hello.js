6d81d1a4e998bca60e974a95ed9a1ab60290c241
Sanjeev Thapa <116951750+sanjeevlcc@users.noreply.github.com>
2024-03-16 12:09:46 +0000

    v11

--- java_script_workload/2_flat_host_noDocker/a_node/hello.js
+++ java_script_workload/2_flat_host_noDocker/a_node/hello.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end('Hello World!, running fro a flat machine \n');
 });
 
 server.listen(port, hostname, () => {
